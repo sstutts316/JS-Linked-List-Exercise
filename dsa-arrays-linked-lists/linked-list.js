057c145bb514fe956f4a4fd3834125d8088cffc3
Steven Stutts <86498128+sstutts316@users.noreply.github.com>
2024-02-03 20:26:59 -0600

    Add files via upload

---
+++ dsa-arrays-linked-lists/linked-list.js
@@ -0,0 +1,151 @@
+/** Node: node for a singly linked list. */
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** LinkedList: chained together nodes. */
+class LinkedList {
+  constructor(vals = []) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+
+    for (let val of vals) this.push(val);
+  }
+
+  /** push(val): add new value to end of list. */
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  /** unshift(val): add new value to start of list. */
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+  }
+
+  /** pop(): return & remove last item. */
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current.val;
+  }
+
+  /** shift(): return & remove first item. */
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = this.head.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead.val;
+  }
+
+  /** getAt(idx): get val at idx. */
+  getAt(idx) {
+    if (idx < 0 || idx >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== idx) {
+      current = current.next;
+      counter++;
+    }
+    return current.val;
+  }
+
+  /** setAt(idx, val): set val at idx to val */
+  setAt(idx, val) {
+    if (idx < 0 || idx >= this.length) return false;
+    let current = this.head;
+    let counter = 0;
+    while (counter !== idx) {
+      current = current.next;
+      counter++;
+    }
+    current.val = val;
+    return true;
+  }
+
+  /** insertAt(idx, val): add node w/val before idx. */
+  insertAt(idx, val) {
+    if (idx < 0 || idx > this.length) return false;
+    if (idx === 0) return !!this.unshift(val);
+    if (idx === this.length) return !!this.push(val);
+
+    let newNode = new Node(val);
+    let previous = this.head;
+    let counter = 0;
+    while (counter !== idx - 1) {
+      previous = previous.next;
+      counter++;
+    }
+    newNode.next = previous.next;
+    previous.next = newNode;
+    this.length++;
+    return true;
+  }
+
+  /** removeAt(idx): return & remove item at idx. */
+  removeAt(idx) {
+    if (idx < 0 || idx >= this.length) return undefined;
+    if (idx === 0) return this.shift();
+    if (idx === this.length - 1) return this.pop();
+
+    let previous = this.head;
+    let counter = 0;
+    while (counter !== idx - 1) {
+      previous = previous.next;
+      counter++;
+    }
+    let removed = previous.next;
+    previous.next = removed.next;
+    this.length--;
+    return removed.val;
+  }
+
+  /** average(): return an average of all values in the list */
+  average() {
+    if (!this.head) return 0;
+    let sum = 0;
+    let current = this.head;
+    while (current) {
+      sum += current.val;
+      current = current.next;
+    }
+    return sum / this.length;
+  }
+}
+
+module.exports = LinkedList;
